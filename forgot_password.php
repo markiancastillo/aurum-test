00f31d25a8f1614874417b508712161347891cc2
Mark Castillo <markian.castillo@benilde.edu.ph>
2018-03-19 11:04:01 +0800

    Trial 3.1.1
    - placed the forgot password controller on the correct folder
    - removed the include line in the forgot password page
    - removed 2 meta tags in the html for
     the forgot password page

--- forgot_password.php
+++ forgot_password.php
@@ -1,12 +1,10 @@
 <?php
-	include('controllers/forgot_password_controller.php');
+#	include('controllers/forgot_password_controller.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8" />
 
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style-responsive.css">
