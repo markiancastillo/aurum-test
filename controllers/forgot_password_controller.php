b9443655d01c6181984cc5c02ab65e282ac9d8a3
Mark Castillo <markian.castillo@benilde.edu.ph>
2018-03-19 15:20:13 +0800

    Trial 3.14.7
    - moved the phpmailer library inside the controllers folder

--- controllers/forgot_password_controller.php
+++ controllers/forgot_password_controller.php
@@ -14,7 +14,8 @@
 	define('app_path', "${protocol}://${domain}" . '/');
 
 #	require_once(app_path . 'lib/PHPMailer/PHPMailerAutoload.php');
-#	echo 'path: ' . app_path . 'lib/PHPMailer/PHPMailerAutoload.php';
+	require_once('PHPMailer/PHPMailerAutoload.php');
+
 
 	$msgDisplay = "";
 	$msgMismatch = "<div class='alert alert-danger alert-dismissable fade in'>
