2d12e497eafccb8672bf58d6d408ae7e38759891
Mark Castillo <markian.castillo@benilde.edu.ph>
2018-03-19 10:57:01 +0800

    Trial 3.0.0
    Cause of the login page not rendering traced to the forgot password controller include ine.
    
    Added the forgot password page for testing.

---
+++ forgot_password.php
@@ -0,0 +1,101 @@
+<?php
+	include('controllers/forgot_password_controller.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style-responsive.css">
+    <link rel="stylesheet" href="assets/css/style1.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/set1.css">
+
+    <!--Google Fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<form method ="POST">
+    <div id="main-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 left-side">
+                    <header>
+                        <h2><img src="images/aurumlogo.png" heigh="180" width="180" ></h2>  
+                    </header>
+                </div>
+                <div class="col-md-6 right-side">
+                    <h2>Reyes Francisco Tecson Sabado<br>and Associates</h2>
+                    <br><br><br>
+                    <span class="input input--hoshi">
+                        <input class="input__field input__field--hoshi" type="email" id="inpEmail" name="inpEmail" maxlength="100" />
+                        <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="name">
+                            <span class="input__label-content input__label-content--hoshi">Email</span>   
+                        </label>
+                    </span>
+                    <span class="input input--hoshi">
+                        <input class="input__field input__field--hoshi" type="email" id="inpCEmail" name="inpCEmail" maxlength="100" />
+                        <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="email">
+                            <span class="input__label-content input__label-content--hoshi">Re-enter email</span>
+                        </label>
+                    </span>
+                    <?php echo $msgDisplay; ?>
+                    <div class="cta">
+                        <button class="btn btn-primary pull-left" type="submit" id="btnReset" name="btnReset">
+                            Reset Password
+                        </button>
+                        <span><a href="login.php">Back</a></span>
+                    </div>
+                    <br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/classie.js"></script>
+<script>
+  (function() {
+    // Reference: 
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+      (function() {
+        // Make sure we trim BOM and NBSP
+        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        String.prototype.trim = function() {
+          return this.replace(rtrim, '');
+        };
+      })();
+    }
+
+    [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+      // in case the input is already filled..
+      if( inputEl.value.trim() !== '' ) {
+        classie.add( inputEl.parentNode, 'input--filled' );
+      }
+
+      // events:
+      inputEl.addEventListener( 'focus', onInputFocus );
+      inputEl.addEventListener( 'blur', onInputBlur );
+    } );
+
+    function onInputFocus( ev ) {
+      classie.add( ev.target.parentNode, 'input--filled' );
+    }
+
+    function onInputBlur( ev ) {
+      if( ev.target.value.trim() === '' ) {
+        classie.remove( ev.target.parentNode, 'input--filled' );
+      }
+    }
+  })();
+</script>
+
+</body>
+</html>
---
+++ forgot_password_controller.php
@@ -0,0 +1,152 @@
+<?php
+	include('config.php');
+	include('security.php');
+	require $_SERVER['DOCUMENT_ROOT'] . '/aurum/lib/PHPMailer/PHPMailerAutoload.php';
+	#require '../PHPMailer/PHPMailerAutoload.php';
+
+	$msgDisplay = "";
+	$msgMismatch = "<div class='alert alert-danger alert-dismissable fade in'>
+						<a href='' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						Email address mismatch. Please make sure that both inputs match.
+					</div>";
+	$msgInvalid = "<div class='alert alert-danger alert-dismissable fade in'>
+						<a href='' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						That email address does not exist. Please check your input and try again.
+					</div>";
+	$msgError = "<div class='alert alert-danger alert-dismissable fade in'>
+						<a href='' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						There was an error sending your reset email. Please try again.
+					</div>";
+	$msgSuccess = "<div class='alert alert-success alert-dismissable fade in'>
+						<a href='' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						A reset link has been sent to your email. Please check your inbox.
+					</div>";
+					
+	if(isset($_POST['btnReset']))
+	{
+		$inpEmail = base64_encode(openssl_encrypt($_POST['inpEmail'], $method, $password, OPENSSL_RAW_DATA, $iv));
+		$inpCEmail = base64_encode(openssl_encrypt($_POST['inpCEmail'], $method, $password, OPENSSL_RAW_DATA, $iv));
+	
+		#email and confirm email must match 
+		if(strcmp(trim($inpEmail), trim($inpCEmail)) == 0)
+		{
+			#match
+			#now check if it exists and belongs to an active account
+			$sql_check = "SELECT COUNT(accountID) AS emailCount FROM accounts WHERE accountEmail = ? AND accountStatus != ?";
+			$params_check = array($inpEmail, 'Archived');
+			$stmt_check = sqlsrv_query($con, $sql_check, $params_check);
+
+			while($row = sqlsrv_fetch_array($stmt_check))
+			{
+				$emailCount = $row['emailCount'];
+			}
+
+			if($emailCount <= 0)
+			{
+				#email does not exist
+				$msgDisplay = $msgInvalid;
+			}
+			else
+			{
+				#email is existing; get the account details and send an email
+				$sql_account = "SELECT accountID, accountFN, accountMN, accountLN, accountEmail FROM accounts WHERE accountEmail = ?";
+				$parmas_account = array($inpEmail);
+				$stmt_account = sqlsrv_query($con, $sql_account, $parmas_account);
+
+				while($det = sqlsrv_fetch_array($stmt_account))
+				{
+					$accountID = base64_encode(openssl_encrypt($det['accountID'], $method, $password, OPENSSL_RAW_DATA, $iv));
+					$accountFN = htmlspecialchars($det['accountFN'], ENT_QUOTES, 'UTF-8');
+					$accountMN = htmlspecialchars($det['accountMN'], ENT_QUOTES, 'UTF-8');
+					$accountLN = htmlspecialchars($det['accountLN'], ENT_QUOTES, 'UTF-8');
+					$accountEmail = htmlspecialchars($det['accountEmail'], ENT_QUOTES, 'UTF-8');
+
+					$displayFN = htmlspecialchars(openssl_decrypt(base64_decode($det['accountFN']), $method, $password, OPENSSL_RAW_DATA, $iv), ENT_QUOTES, 'UTF-8');
+					$displayMN = htmlspecialchars(openssl_decrypt(base64_decode($det['accountMN']), $method, $password, OPENSSL_RAW_DATA, $iv), ENT_QUOTES, 'UTF-8');
+					$displayLN = htmlspecialchars(openssl_decrypt(base64_decode($det['accountLN']), $method, $password, OPENSSL_RAW_DATA, $iv), ENT_QUOTES, 'UTF-8');
+					$displayName = $displayFN . ' ' . $displayLN;
+					$displayEmail = htmlspecialchars(openssl_decrypt(base64_decode($det['accountEmail']), $method, $password, OPENSSL_RAW_DATA, $iv), ENT_QUOTES, 'UTF-8');
+
+					$link = 'http://localhost:8088/aurum/reset_password.php?id=' . urlencode($accountID) . '&request=' . urlencode($accountEmail) . '&user=' . urlencode($accountLN);
+
+					# source:
+					# https://github.com/PHPMailer/PHPMailer/tree/5.2-stable
+					$mail = new PHPMailer;
+				
+					# 0 - disable (default)
+					# 1 - output client messages
+					# 2 - output messages sent by client + from server
+					# 3 - as 2, + information about the initial connection - can help with STARTTLS failures
+					# 4 - as 3, plus even lower level information
+					$mail->SMTPDebug = 0; 
+				
+					$mail->isSMTP();                                      		# Set mailer to use SMTP
+					$mail->Host = 'smtp.gmail.com';  					    	# Specify main and backup SMTP servers
+					$mail->SMTPAuth = true;                                		# Enable SMTP authentication
+					$mail->Username = 'miac.11221127@gmail.com';           		# SMTP username
+					$mail->Password = 'damong_talahiban';                       # SMTP password
+					$mail->SMTPSecure = 'tls';                            		# Enable TLS encryption, `ssl` also accepted
+					$mail->Port = 587;                                    		# TCP port to connect to
+				
+					$mail->setFrom('support@aurum.com', 'Aurum System'); 	  	# sender email that will appear, sender name that will be displayed
+					$mail->addAddress($displayEmail, $displayName);	# the address to which the email will be sent, name is optional
+					
+					$mail->isHTML(true);
+					$mail->Subject = 'Password Reset Request'; 
+					$mail->Body = '
+						Hello ' . $displayName . '!
+						<br /><br />
+						We have received your password reset request. Here is your reset link:
+						<br /><br /> ' .
+						$link
+						. '<br /><br />
+						If you did not request this, you may ignore this email.
+						<br /><br />
+						Regards, <br />
+						Aurum System Team
+					';
+					
+					$mail->AltBody = '
+						Hello ' . $displayName . '!
+
+						We have received your password reset request. Here is your reset link: ' .
+						$link
+
+						. 'If you did not request this, you may ignore this email.
+
+						Regards, 
+						Aurum System Team
+					';
+				
+					# allows insecure connections by using the SMTPOptions property
+					# taken from:
+					# https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting#php-56-certificate-verification-failure
+					$mail->SMTPOptions = array(
+				    	'ssl' => array(
+				    	    'verify_peer' => false,
+				    	    'verify_peer_name' => false,
+				    	    'allow_self_signed' => true
+				    	)
+					);
+	
+					if(!$mail->send()) 
+					{
+						#echo 'Message was not sent.';
+						#echo 'Mailer error: ' . $mail->ErrorInfo;
+						$msgDisplay = $msgError;
+					}
+					else 
+					{
+						#echo 'Message has been sent.';
+						$msgDisplay = $msgSuccess;
+					}
+				}
+			}
+		}
+		else 
+		{
+			#mismatch
+			$msgDisplay = $msgMismatch;
+		}
+	}
+?>
